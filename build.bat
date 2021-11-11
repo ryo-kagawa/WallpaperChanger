592a18c5041c41a6eca34924949c7bd526887235
賀川　諒 <ryo.kagawa.217@gmail.com>
2021-11-12 01:27:13 +0900

    change: ビルド時のコンフィグファイルの出力先を変更忘れ

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ go get github.com/google/go-licenses
 go-licenses save . --force --save_path build/Windows/licenses
 go build -o build/Windows/WallpaperChanger.exe -trimpath
 go build -o build/Windows/WallpaperChanger-no-console.exe -ldflags -H=windowsgui -trimpath
-COPY config.yaml build\config.yaml
+COPY config.yaml build\Windows\config.yaml
