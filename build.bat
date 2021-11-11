867f0bdfcb82fd6d32c7a28e67b2805e31be3828
賀川　諒 <ryo.kagawa.217@gmail.com>
2021-11-12 01:51:27 +0900

    change: デバッグ情報をバイナリに含めていたので削除

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 go get github.com/google/go-licenses
 go-licenses save . --force --save_path build/Windows/licenses
-go build -o build/Windows/WallpaperChanger.exe -trimpath
-go build -o build/Windows/WallpaperChanger-no-console.exe -ldflags -H=windowsgui -trimpath
+go build -o build/Windows/WallpaperChanger.exe -ldflags "-s -w" -trimpath
+go build -o build/Windows/WallpaperChanger-no-console.exe -ldflags "-s -w -H=windowsgui" -trimpath
 COPY config.yaml build\Windows\config.yaml
