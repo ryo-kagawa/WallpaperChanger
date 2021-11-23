d2542562b2f49589a7552c9fe3e5ab35574c3143
賀川　諒 <ryo.kagawa.217@gmail.com>
2021-11-23 17:53:39 +0900

    improve: ビルド時の出力先ディレクトリー名にプログラム名を含めるようにした

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 go get github.com/google/go-licenses
 go-licenses save . --force --save_path build/Windows/licenses
-go build -o build/Windows/WallpaperChanger.exe -ldflags "-s -w" -trimpath ./cmd/wallpaper-changer
-go build -o build/Windows/WallpaperChanger-no-console.exe -ldflags "-s -w -H=windowsgui" -trimpath ./cmd/wallpaper-changer
-go build -o build/Windows/ConfigCreater.exe -ldflags "-s -w" -trimpath ./cmd/config-creater
+go build -o build/WallpaperChanger-Windows/WallpaperChanger.exe -ldflags "-s -w" -trimpath ./cmd/wallpaper-changer
+go build -o build/WallpaperChanger-Windows/WallpaperChanger-no-console.exe -ldflags "-s -w -H=windowsgui" -trimpath ./cmd/wallpaper-changer
+go build -o build/WallpaperChanger-Windows/ConfigCreater.exe -ldflags "-s -w" -trimpath ./cmd/config-creater
