d679711c445c605feb5f6970846c3ed1ddff3c48
賀川　諒 <ryo.kagawa.217@gmail.com>
2021-12-01 22:22:18 +0900

    bugfix: ビルド時にライセンスを出力できるように修正

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 go get github.com/google/go-licenses
-go-licenses save . --force --save_path build/Windows/licenses
+go-licenses save ./... --force --save_path build/WallpaperChanger-Windows/licenses
 go build -o build/WallpaperChanger-Windows/WallpaperChanger.exe -ldflags "-s -w" -trimpath ./cmd/wallpaper-changer
 go build -o build/WallpaperChanger-Windows/WallpaperChanger-no-console.exe -ldflags "-s -w -H=windowsgui" -trimpath ./cmd/wallpaper-changer
 go build -o build/WallpaperChanger-Windows/ConfigCreater.exe -ldflags "-s -w -H=windowsgui" -trimpath ./cmd/config-creater
