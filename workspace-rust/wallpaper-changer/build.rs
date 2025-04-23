4a575e63b79ea648005321cabc061fbee0e66678
賀川　諒 <ryo.kagawa.217@gmail.com>
2025-04-23 22:39:59 +0900

    [機能開発]RegQueryValueExWをRegGetValueWに変更する #46 change: RustのWallpaperChangerでレジストリ情報を取得する時にRegGetValueWを使用するように変更 formatter: フォーマッター適用

--- workspace-rust/wallpaper-changer/build.rs
+++ workspace-rust/wallpaper-changer/build.rs
@@ -37,7 +37,10 @@ fn main() {
     println!(
         "cargo:rustc-env=VERSION_INFO={}",
         [
-            format!("WallpaperChanger.exe {}", std::env::var("VERSION").unwrap_or(env!("CARGO_PKG_VERSION").to_string())),
+            format!(
+                "WallpaperChanger.exe {}",
+                std::env::var("VERSION").unwrap_or(env!("CARGO_PKG_VERSION").to_string())
+            ),
             format!("rustc {}", rustc_version),
             format!("{}", metadata_dependencies)
         ]
